385d5c00d4754aaeb50d80eb98805c4365511000
Eros Smith <eros@utexas.edu>
2016-02-07 20:48:42 -0600

    appears to be working still get to get error in the beautify function

---
+++ src/github.com/ErosSmith/beautify_brackets/beautify_brackets.go
@@ -0,0 +1,112 @@
+package beautify_brackets
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+func getMatchingBracket(openBracket string) (string, error) {
+	switch openBracket {
+	case "{":
+		return "}", nil
+	case "[":
+		return "]", nil
+	case "(":
+		return ")", nil
+	default:
+		return "", errors.New("Not an open bracket")
+
+	}
+}
+
+func getBracketedText(text string) (int, error) {
+	//checking required intial conditions
+	fmt.Println("inside getBrackedText")
+	if len(text) < 1 {
+		return -1, errors.New("Empty string")
+	}
+
+	first_char := text[0:1]
+
+	open_brackets := "{[("
+
+	if !strings.Contains(open_brackets, first_char) {
+		return -1, errors.New("First character isn't open bracket")
+	}
+	//intial conditions correct
+	matching_bracket, _ := getMatchingBracket(first_char)
+	inbetween_bracket := true
+	i_index := -1
+	m_index := -1
+	for inbetween_bracket {
+		fmt.Println(text)
+		m_index = strings.Index(text, matching_bracket)
+		if m_index == -1 {
+			return -1, errors.New("No matching bracket in text")
+		}
+		i_index = strings.Index(text[1:], first_char)
+		i_index = i_index + 1
+		//no in between bracket in text
+		if i_index == 0 {
+			inbetween_bracket = false
+			break
+		}
+		//in between bracket in text remove both brackets
+		if i_index < m_index {
+			text = text[:i_index] + "*" + text[i_index+1:m_index] + "*" + text[m_index+1:]
+		} else {
+			break
+		}
+
+	}
+	fmt.Println(text, m_index)
+	return m_index, nil
+}
+
+func indentLevel(text string) string {
+	fmt.Println("inside indent")
+	n_split := strings.Split(text, "\n")
+
+	for i, s := range n_split {
+		n_split[i] = "\t" + s
+	}
+	return strings.Join(n_split, "\n")
+}
+
+func beautifyBrackededText(text string) string {
+	fmt.Println(text, "in beautify")
+
+	if len(text) < 3 {
+		return text
+	}
+
+	open_brackets := "{[("
+	first_bracket_index := strings.IndexAny(text, open_brackets)
+	if !(first_bracket_index > -1) {
+		return text
+	}
+
+	closing_bracket_index, err := getBracketedText(text[first_bracket_index:])
+
+	if err != nil {
+		return ""
+	}
+
+	pre_bracket_text := text[:first_bracket_index]
+	fmt.Println("pre_bracket_text", pre_bracket_text)
+	bracketed_text := text[first_bracket_index : closing_bracket_index+1+len(pre_bracket_text)]
+	fmt.Println("bracketed_text", bracketed_text)
+	remainder := text[closing_bracket_index+1+len(pre_bracket_text) : len(text)]
+	fmt.Println("remainder", remainder)
+
+	inside := beautifyBrackededText(bracketed_text[1 : len(bracketed_text)-1])
+	fixed_text := bracketed_text[0:1] + "\n" + indentLevel(inside) + "\n" + bracketed_text[len(bracketed_text)-1:len(bracketed_text)]
+
+	remainder = beautifyBrackededText(remainder)
+	output := pre_bracket_text + fixed_text + remainder
+
+	fmt.Println("output", output)
+
+	return output
+}
---
+++ src/github.com/ErosSmith/beautify_brackets/beautify_test.go
@@ -0,0 +1,104 @@
+package beautify_brackets
+
+import (
+	"testing"
+)
+
+func TestGetMatchingBracket(t *testing.T) {
+	s, err := getMatchingBracket("")
+	correct := ""
+	if err == nil {
+		t.Errorf("error not working")
+	}
+
+	s, err = getMatchingBracket("{")
+	if err != nil {
+		t.Errorf("error not working")
+	}
+	correct = "}"
+	if s != correct {
+		t.Errorf("Closing bracket not returned. expected %s but got %s", correct, s)
+	}
+
+	s, err = getMatchingBracket("[")
+	if err != nil {
+		t.Errorf("error not working")
+	}
+	correct = "]"
+	if s != correct {
+		t.Errorf("Closing bracket not returned. expected %s but got %s", correct, s)
+	}
+
+	s, err = getMatchingBracket("(")
+	if err != nil {
+		t.Errorf("error not working")
+	}
+	correct = ")"
+	if s != correct {
+		t.Errorf("Closing bracket not returned. expected %s but got %s", correct, s)
+	}
+
+}
+
+func TestGetBracketedText(t *testing.T) {
+	s, err := getBracketedText("")
+
+	if err != nil {
+		t.Log(err.Error())
+	} else {
+		t.Error("Should have had an error")
+	}
+	if s != -1 {
+		t.Errorf("weird")
+	}
+
+	text := "(     ( )  )   "
+	s, err = getBracketedText(text)
+	t.Log(s)
+	t.Log(text[:s+1])
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if s != 11 {
+		t.Error("Wrong index")
+	}
+
+	text = "(  ( ( ))  ( )  )   "
+	s, err = getBracketedText(text)
+	t.Log(s)
+	t.Log(text[:s+1])
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if s != 16 {
+		t.Error("Wrong index")
+	}
+
+}
+
+func TestIndentLevel(t *testing.T) {
+	text := "((\n ))"
+	t.Log(text)
+	i_text := indentLevel(text)
+	t.Log(i_text)
+
+}
+
+func TestBeatifyBracketedText(t *testing.T) {
+	// text := "((()))"
+	// t.Log(text)
+	// i_text := beautifyBrackededText(text)
+	// t.Log(i_text)
+
+	// text = "[((()))(())]"
+	// t.Log(text)
+	// i_text = beautifyBrackededText(text)
+	// t.Log(i_text)
+
+	text := "[((()))sup(hello(hi how are you))]"
+	// text := "sup(hello(hi how are you))"
+	t.Log(text)
+	i_text := beautifyBrackededText(text)
+	t.Log(i_text)
+
+}
